f5456a366459422edb1faa8060c6dcc17acc531b
Edi <edibuenasalud@gmail.com>
2022-06-25 09:13:27 +0200

    Proyecto Finalizado

--- src/components/EditarProducto.js
+++ src/components/EditarProducto.js
@@ -1,47 +1,91 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { editarProductoAction } from '../actions/productoActions'
+import { useNavigate } from 'react-router-dom';
 
 const EditarProducto = () => {
-  return (
-    <div className='row justify-content-center'>
-        <div className='col-md-8'>
-            <div className='card'>
-                <div className='card-body'>
-                    <h2 className='text-center mb-4 font-weight-bold'>
-                        Editar Producto
-                    </h2>
 
-                    <form>
-                        <div className='form-group'>
-                            <label>Nombre Producto</label>
-                            <input
-                                type='text'
-                                className='form-control'
-                                placeholder='Nombre Producto'
-                                name='nombre'
-                            />
-                        </div>
+    const history = useNavigate()
+    const dispatch = useDispatch()
 
-                        <div className='form-group'>
-                            <label>Precio Producto</label>
-                            <input
-                                type='number'
-                                className='form-control'
-                                placeholder='Precio Producto'
-                                name='precio'
-                            />
-                        </div>
+    // nuevo state de producto
+    const [producto, guardarProducto] = useState({
+        nombre: '',
+        precio: ''
+    })
 
-                        <button
-                            type='submit'
-                            className='btn btn-primary font-weight-bold text-uppercase
-                            d-block w-100'
-                        >Guardar Cambios</button>
-                    </form>
+    // producto a editar
+    const productoEditar = useSelector(state => state.productos.productoeditar)
+
+    // llenar el state automáticamente
+    useEffect(() => {
+        guardarProducto(productoEditar)
+    }, [productoEditar])
+
+    // Leer los datos del formulario
+    const onChangeFormulario = e => {
+        guardarProducto({
+            ...producto,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const {nombre, precio} = producto
+
+    const submitEditarProducto = e => {
+        e.preventDefault()
+
+        dispatch( editarProductoAction(producto) )
+        history('/')
+    }
+    
+    return (
+        <div className='row justify-content-center'>
+            <div className='col-md-8'>
+                <div className='card'>
+                    <div className='card-body'>
+                        <h2 className='text-center mb-4 font-weight-bold'>
+                            Editar Producto
+                        </h2>
+
+                        <form
+                            onSubmit={submitEditarProducto}
+                        >
+                            <div className='form-group'>
+                                <label>Nombre Producto</label>
+                                <input
+                                    type='text'
+                                    className='form-control'
+                                    placeholder='Nombre Producto'
+                                    name='nombre'
+                                    value={nombre}
+                                    onChange={onChangeFormulario}
+                                />
+                            </div>
+
+                            <div className='form-group'>
+                                <label>Precio Producto</label>
+                                <input
+                                    type='number'
+                                    className='form-control'
+                                    placeholder='Precio Producto'
+                                    name='precio'
+                                    value={precio}
+                                    onChange={onChangeFormulario}
+                                />
+                            </div>
+
+                            <button
+                                type='submit'
+                                className='btn btn-primary font-weight-bold text-uppercase
+                                d-block w-100'
+                            >Guardar Cambios</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default EditarProducto
--- src/components/NuevoProducto.js
+++ src/components/NuevoProducto.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux/es/exports'
+import { useNavigate } from 'react-router-dom';
 
 // Actions de Redux
 import { crearNuevoProductoAction } from '../actions/productoActions'
+import { mostrarAlerta, ocultarAlertaAction } from '../actions/alertaActions';
 
 const NuevoProducto = () => {
+
+    const history = useNavigate();
 
     // state del componente
     const [nombre, guardarNombre] = useState('')
@@ -12,6 +16,11 @@ const NuevoProducto = () => {
 
     // utilizar use dispatch y te crea una función
     const dispatch = useDispatch()
+
+    // Acceder al state del store
+    const cargando = useSelector(state => state.productos.loading)
+    const error = useSelector(state => state.productos.error)
+    const alerta = useSelector(state => state.alerta.alerta)
 
     // mandar llamar el action de productoAction
     const agregarProducto = producto => dispatch( crearNuevoProductoAction(producto) )
@@ -22,16 +31,27 @@ const NuevoProducto = () => {
 
         // validar formulario
         if(nombre.trim() === '' || precio <= 0) {
+
+            const alerta = {
+                msg: 'Ambos campos son obligatorios',
+                classes: 'alert alert-danger text-center text-uppercase p3'
+            }
+            dispatch( mostrarAlerta(alerta) )
+            
             return
         }
 
         // si no hay errores
+        dispatch( ocultarAlertaAction() )
 
         // crear el nuevo producto
         agregarProducto({
             nombre,
             precio
         })
+
+        // redireccionar
+        history('/')
     }
 
     return (
@@ -42,6 +62,8 @@ const NuevoProducto = () => {
                         <h2 className='text-center mb-4 font-weight-bold'>
                             Agregar Nuevo Producto
                         </h2>
+
+                        {alerta ? <p className={alerta.classes}> {alerta.msg} </p> : null}
 
                         <form
                             onSubmit={submitNuevoProducto}
@@ -76,6 +98,10 @@ const NuevoProducto = () => {
                                 d-block w-100'
                             >Agregar</button>
                         </form>
+
+                        {cargando ? <p>Cargando...</p> : null}
+                        {error ? <p className='alert alert-danger p2 mt-4 text-center'>Hubo un error</p> :
+                        null}
                     </div>
                 </div>
             </div>
